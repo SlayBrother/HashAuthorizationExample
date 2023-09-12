89f2eb0bfb49d86c3cd18fc6d18bfea8a6cbb4b0
CJ B <cj@DESKTOP-56GPSTK>
2023-09-12 18:10:24 -0500

    built out for tweets

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db, User
-# from forms import Registration, LoginForm 
+from models import connect_db, db, User, Tweet
+from forms import UserForm
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///auth_demo"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_demo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "abc123"
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+app.app_context().push()
 
 connect_db(app)
 db.create_all()
@@ -18,3 +20,50 @@ toolbar = DebugToolbarExtension(app)
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
+@app.route('/tweets')
+def show_tweets():
+
+    if "user_id" not in session:
+        flash("Please Login First!")
+        return redirect('/')
+    return render_template("tweets.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        new_user = User.register(username, password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
+        flash('Welcome! Successfully Created your Account!')
+        return redirect('/tweets')
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        
+        user = User.authenticate(username, password)
+        if user:
+            flash(f'Welcome back {user.username}')
+            session['user_id'] = user.id
+            return redirect('/tweets')
+        else:
+            form.username.errors = ['Invalid username/password']
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_user():
+    session.pop('user_id')
+    flash("Goodbye")
+    return redirect('/')
--- models.py
+++ models.py
@@ -3,9 +3,22 @@ from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 
+bcrypt = Bcrypt()
+
 def connect_db(app):
+    """Connect to databse"""
     db.app = app
     db.init_app(app)
+
+class Tweet(db.Model):
+    __tablename__ = 'tweets'
+
+    id = db.Column(db.Integer, primary_key= True, autoincrement=True)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref="tweets")
+
 
 class User(db.Model):
 
@@ -16,3 +29,29 @@ class User(db.Model):
     username = db.Column(db.Text, nullable=False, unique=True)
 
     password = db.Column(db.Text, nullable=False)
+
+    @classmethod
+    def register(cls, username, pwd):
+        """Register user w/hashed password & return user."""
+
+        hashed = bcrypt.generate_password_hash(pwd)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        # return instance opf user w/username and hashed pwd
+        return cls(username=username, password=hashed_utf8)
+    
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """ Validate that user exists & password is correct
+        
+        return user if valid; else return False
+        """
+
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            # return user instance
+            return u
+        else:
+            return False
